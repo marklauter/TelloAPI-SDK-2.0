ac29750c05bbfdd985cfe12068e192d9d746b5eb
Mark Lauter <mark.lauter@sumosoftware.com>
2019-02-03 03:33:53 -0500

    removed arg checks from emulator service factory

--- src/Tello.Emulator.SDKV2/ServiceFactory.cs
+++ src/Tello.Emulator.SDKV2/ServiceFactory.cs
@@ -9,36 +9,11 @@ namespace Tello.Emulator.SDKV2
 
         public IMessenger CreateMessenger(params object[] args)
         {
-            if (args == null || args.Length == 0)
-            {
-                throw new ArgumentNullException(nameof(args));
-            }
-
-            if (!(args[0] is string))
-            {
-                throw new ArgumentException($"{nameof(args)}[0] should be type of string.");
-            }
-
-            if (!(args[1] is int))
-            {
-                throw new ArgumentException($"{nameof(args)}[1] should be type of int.");
-            }
-
             return _tello;
         }
 
         public IReceiver<INotification> CreateReceiver(params object[] args)
         {
-            if (args == null || args.Length == 0)
-            {
-                throw new ArgumentNullException(nameof(args));
-            }
-
-            if (!(args[0] is int))
-            {
-                throw new ArgumentException($"{nameof(args)}[0] should be type of int.");
-            }
-
             return _tello;
         }
     }
