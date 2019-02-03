062d1b0e70b0add821ea72afd30099474c1bb4f2
Mark Lauter <mark.lauter@sumosoftware.com>
2019-02-02 20:59:35 -0500

    made UDP classes implment Tello.Messenger contracts

--- src/Tello.Udp/Messenger.cs
+++ src/Tello.Udp/Messenger.cs
@@ -9,9 +9,9 @@ using Tello.Messaging;
 
 namespace Tello.Udp
 {
-    public sealed class UdpMessenger : IMessenger
+    public sealed class Messenger : IMessenger
     {
-        public UdpMessenger(string ip, int port, TimeSpan responseTimeout) : base()
+        public Messenger(string ip, int port) : base()
         {
             if (String.IsNullOrEmpty(ip))
             {
@@ -19,15 +19,12 @@ namespace Tello.Udp
             }
 
             _endPoint = new IPEndPoint(IPAddress.Parse(ip), port);
-
-            _responseTimeout = responseTimeout;
         }
 
         public MessengerStates State { get; private set; } = MessengerStates.Disconnected;
         public void ClearError() { State = MessengerStates.Disconnected; }
 
         private readonly IPEndPoint _endPoint;
-        private readonly TimeSpan _responseTimeout;
         private UdpClient _client = null;
 
         public void Connect(TimeSpan timeout, Action<IMessenger> continuation = null)
@@ -35,27 +32,18 @@ namespace Tello.Udp
             if (State == MessengerStates.Disconnected)
             {
                 State = MessengerStates.Connecting;
-                try
-                {
-                    if (!IsNetworkAvailable)
-                    {
-                        Debug.WriteLine("NetworkUnavailableException");
-                        throw new NetworkUnavailableException("Device must be connected to Tello's WIFI.");
-                    }
 
-                    _client?.Dispose();
-                    _client = new UdpClient();
-                    _client.Connect(_endPoint);
-                    State = MessengerStates.Connected;
-
-                    continuation?.Invoke(this);
-                }
-                catch (Exception ex)
+                if (!IsNetworkAvailable)
                 {
-                    State = MessengerStates.Error;
-                    Debug.WriteLine(ex);
-                    throw;
+                    throw new NetworkUnavailableException("Device must be connected to Tello's WIFI.");
                 }
+
+                _client?.Dispose();
+                _client = new UdpClient();
+                _client.Connect(_endPoint);
+                State = MessengerStates.Connected;
+
+                continuation?.Invoke(this);
             }
         }
 
@@ -63,13 +51,13 @@ namespace Tello.Udp
         {
             if (State != MessengerStates.Disconnected)
             {
+                State = MessengerStates.Disconnected;
                 if (_client != null)
                 {
                     _client.Close();
                     _client.Dispose();
                     _client = null;
                 }
-                State = MessengerStates.Disconnected;
                 continuation?.Invoke(this);
             }
         }
@@ -78,31 +66,22 @@ namespace Tello.Udp
 
         public Task<IResponse> SendAsync(IRequest request)
         {
-            if (State != MessengerStates.Connected)
-            {
-                return Response.FromFailure("Unable to send request.")
-            }
-
             return Task.Run(async () =>
             {
-                var timer = new Stopwatch();
+                if (State != MessengerStates.Connected)
+                {
+                    return Response.FromFailure(request, "Unable to send request.");
+                }
+
+                var spinWait = new SpinWait();
+                var timer = Stopwatch.StartNew();
                 try
                 {
-                    if (_client == null || !IsConnected)
-                    {
-                        return Response.FromFailure("not connected");
-                    }
-
-                    var spinWait = new SpinWait();
-                    timer.Start();
-
-                    await _client.SendAsync(datagram, datagram.Length);
-
-                    while (_client.Available == 0 && timer.Elapsed <= _responseTimeout)
+                    await _client.SendAsync(request.Data, request.Data.Length);
+                    while (_client.Available == 0 && timer.Elapsed <= request.Timeout)
                     {
                         spinWait.SpinOnce();
                     }
-
                     timer.Stop();
 
                     if (_client.Available == 0)
@@ -110,11 +89,12 @@ namespace Tello.Udp
                         throw new TimeoutException(_endPoint.ToString());
                     }
 
-                    return Response.FromUdpReceiveResult(await _client.ReceiveAsync(), timer.ElapsedMilliseconds);
+                    var response = await _client.ReceiveAsync();
+                    return Response.FromUdpReceiveResult(request, response.Buffer, timer.Elapsed);
                 }
                 catch (Exception ex)
                 {
-                    return Response.FromException(ex, timer.ElapsedMilliseconds);
+                    return Response.FromException(request, ex, timer.Elapsed);
                 }
             });
         }
---
+++ src/Tello.Udp/Receiver.cs
@@ -0,0 +1,54 @@
+﻿using System;
+using System.Net.Sockets;
+using System.Threading.Tasks;
+using Tello.Messaging;
+
+namespace Tello.Udp
+{
+    public sealed class Receiver : IReceiver
+    {
+        public Receiver(int port) 
+        {
+            _port = port;
+        }
+
+        private readonly int _port;
+
+        public ReceiverStates State { get; private set; }
+
+        public async void Listen(Action<IReceiver, INotification> messageHandler, Action<IReceiver, Exception> errorHandler)
+        {
+            State = ReceiverStates.Listening;
+
+            await Task.Run(async () =>
+            {
+                using (var client = new UdpClient(_port))
+                {
+                    while (State == ReceiverStates.Listening)
+                    {
+                        try
+                        {
+                            var receiveResult = await client.ReceiveAsync();
+                            var message = Notification.FromData(receiveResult.Buffer);
+                            messageHandler?.Invoke(this, message);
+                            if (message.Reply != null)
+                            {
+                                await client.SendAsync(message.Reply, message.Reply.Length, receiveResult.RemoteEndPoint);
+                            }
+                        }
+                        catch(Exception ex)
+                        {
+                            errorHandler?.Invoke(this, ex);
+                        }
+                    }
+                }
+            });
+        }
+
+        public void Stop(Action<IMessenger> continuation)
+        {
+            State = ReceiverStates.Stopped;
+        }
+
+    }
+}
