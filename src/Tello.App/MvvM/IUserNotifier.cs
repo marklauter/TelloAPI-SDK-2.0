939456b04e080ddfbbf1bdfca130492e2297988f
Mark Lauter <mark.lauter@sumosoftware.com>
2019-04-06 14:28:50 -0400

    added user notifier

---
+++ src/Tello.App/MvvM/IUserNotifier.cs
@@ -0,0 +1,8 @@
+﻿namespace Tello.App.MvvM
+{
+    public interface IUserNotifier
+    {
+        void Error(string message, string title = "error");
+        int Input(string message, string title = "user input required");
+    }
+}
