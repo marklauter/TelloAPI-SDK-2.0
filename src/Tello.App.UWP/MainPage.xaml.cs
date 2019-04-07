7e53bef888340b0f4dfef780ef1f3cb38755de59
Mark Lauter <mark.lauter@sumosoftware.com>
2019-04-07 19:11:36 -0400

    stubbed out the flight control and flight state view panels

--- src/Tello.App.UWP/MainPage.xaml.cs
+++ src/Tello.App.UWP/MainPage.xaml.cs
@@ -1,16 +1,19 @@
-﻿using System;
-using System.Collections.Generic;
-using System.IO;
-using System.Linq;
-using System.Runtime.InteropServices.WindowsRuntime;
-using Windows.Foundation;
-using Windows.Foundation.Collections;
-using Windows.UI.Xaml;
+﻿#define EMULATOR
+
+#if EMULATOR
+using Tello.Emulator.SDKV2;
+#else
+using Tello.Udp;
+#endif
+
+using System.Threading;
+using Tello.App.MvvM;
+using Tello.App.UWP.Services;
+using Tello.App.ViewModels;
+using Tello.Controller;
+using Tello.Messaging;
+using Tello.Repository;
 using Windows.UI.Xaml.Controls;
-using Windows.UI.Xaml.Controls.Primitives;
-using Windows.UI.Xaml.Data;
-using Windows.UI.Xaml.Input;
-using Windows.UI.Xaml.Media;
 using Windows.UI.Xaml.Navigation;
 
 // The Blank Page item template is documented at https://go.microsoft.com/fwlink/?LinkId=402352&clcid=0x409
@@ -22,9 +25,63 @@ namespace Tello.App.UWP
     /// </summary>
     public sealed partial class MainPage : Page
     {
+        private readonly IMessengerService _tello;
+        private readonly FlightController _flightController;
+        private readonly IUIDispatcher _uiDispatcher;
+        private readonly IUserNotifier _userNotifier;
+        private readonly IRepository _repository;
+
+        public TelloControllerViewModel ControllerViewModel { get; }
+        public TelloStateViewModel StateViewModel { get; }
+
         public MainPage()
         {
-            this.InitializeComponent();
+            InitializeComponent();
+
+            DataContext = this;
+
+            _uiDispatcher = new UIDispatcher(SynchronizationContext.Current);
+            _userNotifier = new UserNotifier();
+            _repository = new ObservationRepository();
+
+#if EMULATOR
+            var tello = new TelloEmulator();
+#else
+            var tello = new UdpMessenger();
+#endif
+            _tello = tello;
+
+            _flightController = new FlightController(tello, tello.StateServer, tello.VideoServer, tello.VideoSampleProvider);
+
+            ControllerViewModel = new TelloControllerViewModel(_uiDispatcher, _userNotifier, _flightController);
+            StateViewModel = new TelloStateViewModel(_uiDispatcher, _userNotifier, _flightController, _repository);
+
+            ControllerGrid.DataContext = ControllerViewModel;
+            StateGrid.DataContext = StateViewModel;
+        }
+
+        protected override void OnNavigatedTo(NavigationEventArgs e)
+        {
+            base.OnNavigatedTo(e);
+
+#if EMULATOR
+            (_tello as TelloEmulator).PowerOn();
+#endif
+
+            ControllerViewModel.Open(new OpenEventArgs());
+            StateViewModel.Open(new OpenEventArgs());
+        }
+
+        protected override void OnNavigatedFrom(NavigationEventArgs e)
+        {
+            ControllerViewModel.Close();
+            StateViewModel.Close();
+
+#if EMULATOR
+            (_tello as TelloEmulator).PowerOff();
+#endif
+
+            base.OnNavigatedFrom(e);
         }
     }
 }
