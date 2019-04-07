af3cdf79914208538acaec907220ca5ad59ecd9e
Mark Lauter <mark.lauter@sumosoftware.com>
2019-04-07 19:11:12 -0400

    added mvvm service implementation stubs

---
+++ src/Tello.App.UWP/Services/UserNotifier.cs
@@ -0,0 +1,20 @@
+﻿using System;
+using System.Diagnostics;
+using Tello.App.MvvM;
+
+namespace Tello.App.UWP.Services
+{
+    public class UserNotifier : IUserNotifier
+    {
+        public void Error(string message, string title = "error")
+        {
+            Debug.Write($"{DateTime.Now.TimeOfDay} - {title}: {message}");
+        }
+
+        public int Input(string message, string title = "user input required")
+        {
+            Debug.Write($"{DateTime.Now.TimeOfDay} - {title}: {message}");
+            return 0;
+        }
+    }
+}
