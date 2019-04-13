2d174f69095cd08fd30fa5b452472609c598d6f6
Mark Lauter <mark.lauter@sumosoftware.com>
2019-04-13 15:07:15 -0400

    added useful queries

---
+++ src/Tello.Repository/scripts/UsefulQueries.sql
@@ -0,0 +1,4 @@
+﻿select b.*
+from TelloCommandObservation a
+join AirSpeedObservation b on b.SessionId=a.SessionId and b.Timestamp BETWEEN a.Initiated and a.Completed 
+where a.Id='fc6c3259-6bdb-429f-a5a4-400f1af039cc'
