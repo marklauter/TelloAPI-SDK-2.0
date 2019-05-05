c57af30cf275e38e841285262b2d29fbb71a8be5
Mark Lauter <mark.lauter@sumosoftware.com>
2019-05-04 23:34:59 -0400

    added test support classes

---
+++ tests/Tello.Observations.Sqlite.Test/Support/TestSupportClasses.cs
@@ -0,0 +1,44 @@
+﻿using Tello.Messaging;
+
+namespace Tello.Observations.Sqlite.Test
+{
+    public class TestPosition : IPosition
+    {
+        public int AltitudeAGLInCm { get; } = 1;
+        public double AltitudeMSLInCm { get; } = 1;
+        public int Heading { get; } = 1;
+        public double X { get; } = 1;
+        public double Y { get; } = 1;
+    }
+
+    public class TestAttitude : IAttitude
+    {
+        public int Pitch { get; } = 1;
+        public int Roll { get; } = 1;
+        public int Yaw { get; } = 1;
+    }
+
+    public class TestAirSpeed : IAirSpeed
+    {
+        public int SpeedX { get; } = 1;
+        public int SpeedY { get; } = 1;
+        public int SpeedZ { get; } = 1;
+        public double AccelerationX { get; } = 1;
+        public double AccelerationY { get; } = 1;
+        public double AccelerationZ { get; } = 1;
+    }
+
+    public class TestBattery : IBattery
+    {
+        public int TemperatureLowC { get; } = 1;
+        public int TemperatureHighC { get; } = 1;
+        public int PercentRemaining { get; } = 1;
+    }
+
+    public class TestHobbsMeter : IHobbsMeter
+    {
+        public int DistanceTraversedInCm { get; } = 1;
+        public int MotorTimeInSeconds { get; } = 1;
+        public double FlightTimeRemainingInMinutes => (15 * 60 - MotorTimeInSeconds) / 60.0;
+    }
+}
