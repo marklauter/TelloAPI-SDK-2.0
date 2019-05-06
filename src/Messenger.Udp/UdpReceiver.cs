1291d74a3899e6f1f3acf17b25dd0fb419cf0fee
Mark Lauter <mark.lauter@sumosoftware.com>
2019-05-05 22:27:23 -0400

    added udp implementation of new messenger services

---
+++ src/Messenger.Udp/UdpReceiver.cs
@@ -0,0 +1,49 @@
+﻿using System;
+using System.Net.Sockets;
+using System.Threading;
+using System.Threading.Tasks;
+
+namespace Messenger.Udp
+{
+    public sealed class UdpReceiver : Receiver
+    {
+        private readonly int _port;
+
+        public UdpReceiver(int port)
+        {
+            _port = port;
+        }
+
+        protected override async Task Listen(CancellationToken cancellationToken)
+        {
+            if (cancellationToken == null)
+            {
+                throw new ArgumentNullException(nameof(cancellationToken));
+            }
+
+            var wait = new SpinWait();
+
+            using (var client = new UdpClient(_port))
+            {
+                while (!cancellationToken.IsCancellationRequested)
+                {
+                    try
+                    {
+                        if (client.Available > 0)
+                        {
+                            MessageReceived(new Envelope((await client.ReceiveAsync()).Buffer));
+                        }
+                        else
+                        {
+                            wait.SpinOnce();
+                        }
+                    }
+                    catch (Exception ex)
+                    {
+                        ExceptionThrown(ex);
+                    }
+                }
+            }
+        }
+    }
+}
---
+++ src/Messenger.Udp/UdpTransceiver.cs
@@ -0,0 +1,53 @@
+﻿using Sumo.Retry.Policies;
+using System;
+using System.Diagnostics;
+using System.Net;
+using System.Net.Sockets;
+using System.Threading;
+using System.Threading.Tasks;
+
+namespace Messenger.Udp
+{
+    public sealed class UdpTransceiver : Transceiver, ITransceiver, IDisposable
+    {
+        private readonly IPEndPoint _endPoint;
+        private readonly UdpClient _client = new UdpClient();
+
+        public UdpTransceiver(RetryPolicy retryPolicy, IPAddress ipAddress, int port)
+            : base(retryPolicy)
+        {
+            _endPoint = new IPEndPoint(ipAddress, port);
+        }
+
+        public void Dispose()
+        {
+            _client.Dispose();
+        }
+
+        protected override async Task<IResponse> Send(IRequest request)
+        {
+            var stopwatch = Stopwatch.StartNew();
+
+            await _client.SendAsync(request.Data, request.Data.Length, _endPoint);
+
+            await Task.Run(() =>
+            {
+                var spinWait = new SpinWait();
+                while (_client.Available == 0 && stopwatch.Elapsed <= request.Timeout)
+                {
+                    spinWait.SpinOnce();
+                }
+                stopwatch.Stop();
+            });
+
+            if (_client.Available == 0)
+            {
+                throw new TimeoutException(stopwatch.Elapsed.ToString());
+            }
+
+            var receiveResult = await _client.ReceiveAsync();
+
+            return new Response(request, receiveResult.Buffer, stopwatch.Elapsed);
+        }
+    }
+}
