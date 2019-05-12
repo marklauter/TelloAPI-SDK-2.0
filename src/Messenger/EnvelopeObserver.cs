1e6108ef53f8809792e89d10b852a97456a9a2cf
Mark Lauter <mark.lauter@sumosoftware.com>
2019-05-11 20:23:03 -0400

    added EnvelopeObserver

---
+++ src/Messenger/EnvelopeObserver.cs
@@ -0,0 +1,49 @@
+﻿using System;
+
+namespace Messenger
+{
+    public abstract class EnvelopeObserver : IObserver<IEnvelope>
+    {
+        private IDisposable _unsubscriber;
+
+        public EnvelopeObserver()
+        {
+        }
+
+        public EnvelopeObserver(Receiver receiver)
+        {
+            Subscribe(receiver);
+        }
+
+        public void Subscribe(Receiver receiver)
+        {
+            if (receiver == null)
+            {
+                throw new ArgumentNullException(nameof(receiver));
+            }
+
+            if (_unsubscriber == null)
+            {
+                _unsubscriber = receiver.Subscribe(this);
+            }
+        }
+
+        public void Unsubscribe()
+        {
+            if (_unsubscriber != null)
+            {
+                _unsubscriber.Dispose();
+                _unsubscriber = null;
+            }
+        }
+
+        public void OnCompleted()
+        {
+            Unsubscribe();
+        }
+
+        public abstract void OnError(Exception error);
+
+        public abstract void OnNext(IEnvelope value);
+    }
+}
