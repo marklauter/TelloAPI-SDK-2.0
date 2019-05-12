bf814c354001eee9e8220edbea7e20a3ff77cb5e
Mark Lauter <mark.lauter@sumosoftware.com>
2019-05-12 16:21:56 -0400

    added tello exception

---
+++ src/Tello/TelloException.cs
@@ -0,0 +1,19 @@
+﻿using System;
+
+namespace Tello
+{
+    public class TelloException : Exception
+    {
+        public TelloException()
+        {
+        }
+
+        public TelloException(string message) : base(message)
+        {
+        }
+
+        public TelloException(string message, Exception innerException) : base(message, innerException)
+        {
+        }
+    }
+}
