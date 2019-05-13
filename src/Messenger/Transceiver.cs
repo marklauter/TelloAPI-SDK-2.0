06737fc525d04718ca17e46040fae20966cac428
Mark Lauter <mark.lauter@sumosoftware.com>
2019-05-13 13:43:35 -0400

    removed retry from transceiver

--- src/Messenger/Transceiver.cs
+++ src/Messenger/Transceiver.cs
@@ -1,6 +1,4 @@
-﻿using Sumo.Retry;
-using Sumo.Retry.Policies;
-using System;
+﻿using System;
 using System.Diagnostics;
 using System.Threading.Tasks;
 
@@ -8,23 +6,14 @@ namespace Messenger
 {
     public abstract class Transceiver : ITransceiver
     {
-        public Transceiver(RetryPolicy retryPolicy)
-        {
-            _retryPolicy = retryPolicy ?? throw new ArgumentNullException(nameof(retryPolicy));
-        }
-
-        private readonly RetryPolicy _retryPolicy;
-
         public async Task<IResponse> SendAsync(IRequest request)
         {
             var stopwatch = Stopwatch.StartNew();
             try
             {
-                return await WithRetry.InvokeAsync(
-                   _retryPolicy,
-                   async () => await Send(request));
+                return await Send(request);
             }
-            catch(Exception ex)
+            catch (Exception ex)
             {
                 return new Response(request, ex, stopwatch.Elapsed);
             }
