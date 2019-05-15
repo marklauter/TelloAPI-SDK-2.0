bbd6e60355b3f165f2df8583ff9ac3b94799dacb
Mark Lauter <mark.lauter@sumosoftware.com>
2019-05-14 21:42:42 -0400

    added a messenger implementation that interacts with the drone simulation

---
+++ src/Messenger.Simulator/SimReciever.cs
@@ -0,0 +1,46 @@
+﻿using System;
+using System.Threading;
+using System.Threading.Tasks;
+using Tello.Simulator.Transmitters;
+
+namespace Messenger.Simulator
+{
+    public sealed class SimReceiver : Receiver
+    {
+        private readonly ITransmitter _transmitter;
+
+        public SimReceiver(ITransmitter transmitter)
+        {
+            _transmitter = transmitter ?? throw new ArgumentNullException(nameof(transmitter));
+        }
+
+        protected override async Task Listen(CancellationToken cancellationToken)
+        {
+            if (cancellationToken == null)
+            {
+                throw new ArgumentNullException(nameof(cancellationToken));
+            }
+
+            var wait = new SpinWait();
+
+            while (!cancellationToken.IsCancellationRequested)
+            {
+                try
+                {
+                    if (_transmitter.Available > 0)
+                    {
+                        MessageReceived(new Envelope((await _transmitter.ReceiveAsync()).Buffer));
+                    }
+                    else
+                    {
+                        wait.SpinOnce();
+                    }
+                }
+                catch (Exception ex)
+                {
+                    ExceptionThrown(ex);
+                }
+            }
+        }
+    }
+}
---
+++ src/Messenger.Simulator/SimTransceiver.cs
@@ -0,0 +1,29 @@
+﻿using System;
+using System.Diagnostics;
+using System.Threading.Tasks;
+using Tello.Simulator;
+
+namespace Messenger.Simulator
+{
+    public sealed class SimTransceiver : Transceiver, ITransceiver
+    {
+        private readonly Drone _droneSim;
+
+        public SimTransceiver(Drone droneSim)
+        {
+            _droneSim = droneSim ?? throw new ArgumentNullException(nameof(droneSim));
+        }
+
+        protected override async Task<IResponse> Send(IRequest request)
+        {
+            var stopwatch = Stopwatch.StartNew();
+            return new Response(
+                request,
+                await _droneSim.Invoke(request.Data),
+                stopwatch.Elapsed);
+        }
+
+        public override void Dispose() { }
+    }
+}
+
