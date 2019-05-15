901c2c4f7a3c178dc8088003099d2ebe97e408ba
Mark Lauter <mark.lauter@sumosoftware.com>
2019-05-14 22:37:33 -0400

    added reference code from dead project

---
+++ src/Tello.Simulator/DEAD_REFERENCE_CODE/CommandInterpreter.cs
@@ -0,0 +1,273 @@
+﻿//using System;
+//using System.Threading.Tasks;
+//using Tello.Messaging;
+
+//namespace Tello.Emulator.SDKV2
+//{
+//    internal sealed class CommandInterpreter
+//    {
+//        public CommandInterpreter(StateManager stateManager)
+//        {
+//            _stateManager = stateManager ?? throw new ArgumentNullException(nameof(stateManager));
+//        }
+
+//        private readonly StateManager _stateManager;
+//        private readonly string _ok = "ok";
+//        private readonly string _error = "error";
+
+//        public string Interpret(string message)
+//        {
+//            try
+//            {
+//                var command = CommandParser.GetCommand(message);
+//                if (!_stateManager.IsPoweredUp)
+//                {
+//                    Log.WriteLine($"not powered up. Message ignored: {message}");
+//                    return null;
+//                }
+//                if (!_stateManager.IsSdkModeActivated && command != TelloCommands.EnterSdkMode)
+//                {
+//                    Log.WriteLine($"not in SDK mode. Message ignored: {message}");
+//                    return null;
+//                }
+//                Log.WriteLine($"message received: {message}, command identified: {command}");
+
+//                var args = CommandParser.GetArgs(message);
+//                switch (command)
+//                {
+//                    case TelloCommands.EnterSdkMode:
+//                        if (!_stateManager.IsSdkModeActivated)
+//                        {
+//                            _stateManager.EnterSdkMode();
+//                            if (_stateManager.IsSdkModeActivated)
+//                            {
+//                                return _ok;
+//                            }
+//                            else
+//                            {
+//                                return _error;
+//                            }
+//                        }
+//                        return null;
+//                    case TelloCommands.Takeoff:
+//                        _stateManager.TakeOff();
+//                        return _ok;
+//                    case TelloCommands.Land:
+//                        _stateManager.Land();
+//                        return _ok;
+//                    case TelloCommands.StartVideo:
+//                        _stateManager.StartVideo();
+//                        return _ok;
+//                    case TelloCommands.StopVideo:
+//                        _stateManager.StopVideo();
+//                        return _ok;
+//                    case TelloCommands.Stop:
+//                        return _ok;
+//                    case TelloCommands.EmergencyStop:
+//                        _stateManager.EmergencyStop();
+//                        return _ok;
+//                    case TelloCommands.Up:
+//                        if (args.Length != 1)
+//                        {
+//                            return _error;
+//                        }
+//                        _stateManager.GoUp(Int32.Parse(args[0]));
+//                        return _ok;
+//                    case TelloCommands.Down:
+//                        if (args.Length != 1)
+//                        {
+//                            return _error;
+//                        }
+//                        _stateManager.GoDown(Int32.Parse(args[0]));
+//                        return _ok;
+//                    case TelloCommands.Left:
+//                        if (args.Length != 1)
+//                        {
+//                            return _error;
+//                        }
+//                        _stateManager.GoLeft(Int32.Parse(args[0]));
+//                        return _ok;
+//                    case TelloCommands.Right:
+//                        if (args.Length != 1)
+//                        {
+//                            return _error;
+//                        }
+//                        _stateManager.GoRight(Int32.Parse(args[0]));
+//                        return _ok;
+//                    case TelloCommands.Forward:
+//                        if (args.Length != 1)
+//                        {
+//                            return _error;
+//                        }
+//                        _stateManager.GoForward(Int32.Parse(args[0]));
+//                        return _ok;
+//                    case TelloCommands.Back:
+//                        if (args.Length != 1)
+//                        {
+//                            return _error;
+//                        }
+//                        _stateManager.GoBack(Int32.Parse(args[0]));
+//                        return _ok;
+//                    case TelloCommands.ClockwiseTurn:
+//                        if (args.Length != 1)
+//                        {
+//                            return _error;
+//                        }
+//                        _stateManager.TurnClockwise(Int32.Parse(args[0]));
+//                        return _ok;
+//                    case TelloCommands.CounterClockwiseTurn:
+//                        if (args.Length != 1)
+//                        {
+//                            return _error;
+//                        }
+//                        _stateManager.TurnCounterClockwise(Int32.Parse(args[0]));
+//                        return _ok;
+//                    case TelloCommands.Flip:
+//                    {
+//                        if (args.Length != 1)
+//                        {
+//                            return _error;
+//                        }
+//                        // there's no state to manage for zero sum movements
+//                        return _ok;
+//                    }
+//                    case TelloCommands.Go:
+//                    {
+//                        if (args.Length != 4)
+//                        {
+//                            return _error;
+//                        }
+//                        _stateManager.Go(
+//                            Int32.Parse(args[0]),
+//                            Int32.Parse(args[1]),
+//                            Int32.Parse(args[2]),
+//                            Int32.Parse(args[3]));
+//                        return _ok;
+//                    }
+//                    case TelloCommands.Curve:
+//                    {
+//                        if (args.Length != 7)
+//                        {
+//                            return _error;
+//                        }
+//                        //todo: implement curve in _flightController
+//                        return _ok;
+//                    }
+//                    case TelloCommands.SetSpeed:
+//                    {
+//                        if (args.Length != 1)
+//                        {
+//                            return _error;
+//                        }
+//                        _stateManager.SetSpeed(Int32.Parse(args[0]));
+//                        return _ok;
+//                    }
+//                    case TelloCommands.SetRemoteControl:
+//                    {
+//                        if (args.Length != 4)
+//                        {
+//                            return _error;
+//                        }
+//                        if (Int32.Parse(args[0]) < -100 || Int32.Parse(args[0]) > 100
+//                            || Int32.Parse(args[1]) < -100 || Int32.Parse(args[1]) > 100
+//                            || Int32.Parse(args[2]) < -100 || Int32.Parse(args[2]) > 100
+//                            || Int32.Parse(args[3]) < -100 || Int32.Parse(args[3]) > 100)
+//                        {
+//                            return _error;
+//                        }
+//                        return _ok;
+//                    }
+//                    case TelloCommands.SetWiFiPassword:
+//                    {
+//                        if (args.Length != 2)
+//                        {
+//                            return _error;
+//                        }
+//                        return _ok;
+//                    }
+//                    #region mission pad
+//                    //case Commands.SetMissionPadOn:
+//                    //    //if (_droneState.MissionPadDected == -1)
+//                    //    //{
+//                    //    //    return _error;
+//                    //    //}
+//                    //    _inMissionPadMode = true;
+//                    //    return _ok;
+//                    //case Commands.SetMissionPadOff:
+//                    //    _inMissionPadMode = false;
+//                    //    return _ok;
+//                    //case Commands.SetMissionPadDirection:
+//                    //    if (!_inMissionPadMode)
+//                    //    {
+//                    //        return null;
+//                    //    }
+//                    //    else
+//                    //    {
+//                    //        var args = CommandParser.GetArgs(message);
+//                    //        if (args.Length != 1)
+//                    //        {
+//                    //            return _error;
+//                    //        }
+//                    //        var direction = Int32.Parse(args[0]);
+//                    //        if (direction < 0 || direction > 2)
+//                    //        {
+//                    //            return _error;
+//                    //        }
+//                    //        return _ok;
+//                    //    }
+//                    #endregion
+//                    case TelloCommands.SetStationMode:
+//                    {
+//                        if (args.Length != 2)
+//                        {
+//                            return _error;
+//                        }
+//                        return _ok;
+//                    }
+//                    case TelloCommands.GetSpeed:
+//                    {
+//                        var value = _stateManager.GetSpeed();
+//                        return value != -1
+//                            ? value.ToString()
+//                            : null;
+//                    }
+//                    case TelloCommands.GetBattery:
+//                    {
+//                        var value = _stateManager.GetBattery();
+//                        return value != -1
+//                            ? value.ToString()
+//                            : null;
+//                    }
+//                    case TelloCommands.GetTime:
+//                    {
+//                        var value = _stateManager.GetTime();
+//                        return value != -1
+//                            ? value.ToString()
+//                            : null;
+//                    }
+//                    case TelloCommands.GetWIFISnr:
+//                        return _stateManager.IsPoweredUp
+//                            ? "snr"
+//                            : null;
+//                    case TelloCommands.GetSdkVersion:
+//                        return _stateManager.IsPoweredUp
+//                            ? "SDK 2.0-E"
+//                            : null;
+//                    case TelloCommands.GetSerialNumber:
+//                        return _stateManager.IsPoweredUp
+//                            ? "Tello SDK 2.0 Emulator"
+//                            : null;
+//                    default:
+//                        return _error;
+//                }
+//            }
+//            catch (Exception ex)
+//            {
+//                Log.WriteLine($"{nameof(CommandInterpreter)} - {ex}");
+//                return _stateManager.IsPoweredUp
+//                    ? _error
+//                    : null;
+//            }
+//        }
+//    }
+//}
---
+++ src/Tello.Simulator/DEAD_REFERENCE_CODE/TelloEmulator.cs
@@ -0,0 +1,92 @@
+﻿//using System;
+//using System.Diagnostics;
+//using System.Text;
+//using System.Threading.Tasks;
+//using Tello.Messaging;
+
+//namespace Tello.Emulator.SDKV2
+//{
+//    //https://dl-cdn.ryzerobotics.com/downloads/Tello/Tello%20SDK%202.0%20User%20Guide.pdf
+
+//    //todo: add debug output to every call in the system so the user (programmer) can see what's happening (use the Log static class)
+//    public class TelloEmulator : IMessengerService
+//    {
+//        public TelloEmulator()
+//        {
+//            _droneState = new RawDroneStateEmulated();
+
+//            StateServer = new StateServer(_droneState);
+//            var videoServer = new VideoServer(30, 0);
+//            VideoServer = videoServer;
+//            VideoSampleProvider = videoServer;
+
+//            _stateManager = new StateManager((RawDroneStateEmulated)_droneState, (VideoServer)VideoServer, (StateServer)StateServer);
+//            _commandInterpreter = new CommandInterpreter(_stateManager);
+//        }
+
+//        public IMessageRelayService<IRawDroneState> StateServer { get; }
+//        public IMessageRelayService<IVideoSample> VideoServer { get; }
+//        public IVideoSampleProvider VideoSampleProvider { get; }
+
+//        public Position Position => _stateManager.Position;
+
+//        private readonly IRawDroneState _droneState;
+//        private readonly StateManager _stateManager;
+//        private readonly CommandInterpreter _commandInterpreter;
+
+//        public bool IsPoweredUp => _stateManager.IsPoweredUp;
+
+//        public void PowerOn()
+//        {
+//            if (!IsPoweredUp)
+//            {
+//                _stateManager.PowerOn();
+//                MessengerState = MessengerStates.Disconnected;
+//            }
+//        }
+
+//        public void PowerOff()
+//        {
+//            _stateManager.PowerOff();
+//            MessengerState = MessengerStates.Disconnected;
+//        }
+
+//        #region IMessengerService
+
+//        public MessengerStates MessengerState { get; private set; } = MessengerStates.Disconnected;
+
+//        public void Connect()
+//        {
+//            if (!_stateManager.IsPoweredUp)
+//            {
+//                throw new TelloUnavailableException("Device must be connected to Tello's WIFI.");
+//            }
+
+//            if (MessengerState == MessengerStates.Disconnected)
+//            {
+//                MessengerState = MessengerStates.Connected;
+//            }
+//        }
+
+//        public void Disconnect()
+//        {
+//            MessengerState = MessengerStates.Disconnected;
+//        }
+
+//        public Task<IResponse> SendAsync(IMessage request)
+//        {
+//            if (MessengerState != MessengerStates.Connected)
+//            {
+//                return Task.FromResult(Response.FromException(request, new InvalidOperationException("Not connected."), TimeSpan.FromSeconds(0)));
+//            }
+
+//            var clock = Stopwatch.StartNew();
+//            var message = Encoding.UTF8.GetString(request.Data);
+
+//            var response = _commandInterpreter.Interpret(message);
+            
+//            return Task.FromResult(Response.FromData(request, Encoding.UTF8.GetBytes(response), clock.Elapsed));
+//        }
+//        #endregion
+//    }
+//}
