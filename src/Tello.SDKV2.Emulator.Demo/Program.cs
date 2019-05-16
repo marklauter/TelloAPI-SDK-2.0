cdb8de731b28c67429e8f9aaa2a38843ba9eaf74
Mark Lauter <mark.lauter@sumosoftware.com>
2019-05-15 22:50:26 -0400

    refactored sim demo program for new simulator sub system

--- src/Tello.SDKV2.Emulator.Demo/Program.cs
+++ src/Tello.SDKV2.Emulator.Demo/Program.cs
@@ -1,127 +1,197 @@
-﻿using System;
+﻿using Messenger.Simulator;
+using Repository;
+using Repository.Sqlite;
+using System;
 using System.Threading;
 using Tello.Controller;
+using Tello.Entities;
+using Tello.Entities.Sqlite;
 
-namespace Tello.Emulator.SDKV2.Demo
+namespace Tello.Simulator.SDKV2.Demo
 {
     internal class Program
     {
+        private static readonly DroneSimulator _simulator;
+        private static readonly DroneMessenger _tello;
+        private static readonly IRepository _repository;
+        private static readonly ISession _session;
+
         static Program()
         {
-            _tello = new TelloEmulator();
-            _controller = new FlightController(_tello, _tello.StateServer, _tello.VideoServer, _tello.VideoSampleProvider);
+            _simulator = new DroneSimulator();
+
+            var transceiver = new SimTransceiver(_simulator.MessageHandler);
+            var stateReceiver = new SimReceiver(_simulator.StateTransmitter);
+            var videoReceiver = new SimReceiver(_simulator.VideoTransmitter);
+
+            _tello = new DroneMessenger(transceiver, stateReceiver, videoReceiver);
+
+            _tello.Controller.ConnectionStateChanged += Controller_ConnectionStateChanged;
+            _tello.Controller.ExceptionThrown += Controller_ExceptionThrown;
+            _tello.Controller.ResponseReceived += Controller_ResponseReceived;
+
+            _tello.StateObserver.StateChanged += StateObserver_StateChanged;
+            _tello.VideoObserver.VideoSampleReady += VideoObserver_VideoSampleReady;
+
+            _repository = new SqliteRepository((null, "tello.sim.sqlite"));
+            _repository.CreateCatalog<Session>();
+            _repository.CreateCatalog<ObservationGroup>();
+            _repository.CreateCatalog<StateObservation>();
+            _repository.CreateCatalog<AirSpeedObservation>();
+            _repository.CreateCatalog<AttitudeObservation>();
+            _repository.CreateCatalog<BatteryObservation>();
+            _repository.CreateCatalog<HobbsMeterObservation>();
+            _repository.CreateCatalog<PositionObservation>();
+            _repository.CreateCatalog<ResponseObservation>();
+
+            _session = _repository.NewEntity<Session>();
         }
 
-        private static readonly TelloEmulator _tello;
-        private static readonly FlightController _controller;
+        #region event handlers
 
-        private static void Main(string[] args)
+        private static void Controller_ResponseReceived(object sender, Controller.Events.ResponseReceivedArgs e)
         {
-            _controller.StateChanged += Controller_DroneStateReceived;
-            _controller.ExceptionThrown += Controller_FlightControllerExceptionThrown;
-            _controller.CommandExceptionThrown += Controller_FlightControllerCommandExceptionThrown;
-            _controller.CommandResponseReceived += Controller_FlightControllerResponseReceived;
-            _controller.QueryResponseReceived += Controller_FlightControllerValueReceived;
-            _controller.VideoSampleReady += Controller_VideoSampleReady;
-
-            Log.WriteLine("> power up");
-            _tello.PowerOn();
-
-            Log.WriteLine("> enter sdk mode");
-            _controller.Connect();
-
-            Log.WriteLine("> get battery");
-            _controller.GetBattery();
-
-            var wait = new SpinWait();
-            while (!_canTakeOff)
+            if (!_canMove
+                && (Command)e.Response.Request.Data == Commands.Takeoff
+                && e.Response.Success && e.Response.Message == Responses.Ok.ToString().ToLowerInvariant())
             {
-                wait.SpinOnce();
+                _canMove = true;
             }
 
-            Log.WriteLine("> take off");
-            _controller.TakeOff();
+            Log.WriteLine($"{(Command)e.Response.Request.Data} returned '{e.Response.Message}' in {(int)e.Response.TimeTaken.TotalMilliseconds}ms", ConsoleColor.Cyan);
+            Log.WriteLine($"Estimated Position: { _tello.Controller.Position}", ConsoleColor.Blue);
 
-            try
-            {
-                Log.WriteLine("> go forward failure example");
-                _controller.GoForward(10);
-            }
-            catch (Exception ex)
-            {
-                Log.WriteLine($"GoForward failed with exception {ex.GetType()} and message '{ex.Message}'", ConsoleColor.Red);
-            }
-
-            //Log.WriteLine("> go forward");
-            //_controller.GoForward(50);
-
-            //Log.WriteLine("> go up");
-            //_controller.GoUp(50);
-
-            //Log.WriteLine("> turn counter clockwise");
-            //_controller.TurnCounterClockwise(90);
-
-            //Log.WriteLine("> turn clockwise");
-            //_controller.TurnClockwise(45);
-
-            Log.WriteLine("> fly polygon");
-            _controller.FlyPolygon(4, 100, 50, ClockDirections.Clockwise, false);
-
-            Log.WriteLine("> land");
-            _controller.Land();
-
-            Log.WriteLine("> press any key to end");
-            Console.ReadKey(false);
-
-            _tello.PowerOff();
+            _repository.Insert(new ResponseObservation(e.Response));
         }
 
-        private static void Controller_VideoSampleReady(object sender, VideoSampleReadyArgs e)
+        private static void VideoObserver_VideoSampleReady(object sender, Controller.Events.VideoSampleReadyArgs e)
         {
-            //throw new NotImplementedException();
+            throw new NotImplementedException();
         }
 
-        private static void Controller_FlightControllerValueReceived(object sender, QueryResponseReceivedArgs e)
-        {
-            Log.WriteLine($"{e.ResponseType} == '{e.Value}' in {e.Elapsed.TotalMilliseconds}ms", ConsoleColor.Green);
-        }
-
-        private static void Controller_FlightControllerResponseReceived(object sender, CommandResponseReceivedArgs e)
-        {
-            Log.WriteLine($"{e.Command} returned '{e.Response}' in {e.Elapsed.TotalMilliseconds}ms", ConsoleColor.Magenta, false);
-            Log.WriteLine($"Actual Position: { _tello.Position}", ConsoleColor.Blue, false);
-            Log.WriteLine($"Estimated Position: { _controller.DroneState.Position}", ConsoleColor.Blue);
-        }
-
-        private static void Controller_FlightControllerExceptionThrown(object sender, ExceptionThrownArgs e)
-        {
-            Log.WriteLine($"Exception {e.Exception.GetType()} with message '{e.Exception.Message}'", ConsoleColor.Red, false);
-            Log.WriteLine("| Stack trace", ConsoleColor.Red, false);
-            Log.WriteLine($"| {e.Exception.StackTrace}", ConsoleColor.Red);
-        }
-
-        private static void Controller_FlightControllerCommandExceptionThrown(object sender, CommandExceptionThrownArgs e)
-        {
-            Log.WriteLine($"{e.Command} failed with exception {e.Exception.GetType()} with message '{e.Exception.Message}' after {e.Elapsed.TotalMilliseconds}ms", ConsoleColor.Red, false);
-            Log.WriteLine("| Stack trace", ConsoleColor.Red, false);
-            Log.WriteLine($"| {e.Exception.StackTrace}", ConsoleColor.Red);
-        }
-
-        private static bool _canTakeOff = false;
         private static int _stateCount = 0;
-        private static void Controller_DroneStateReceived(object sender, StateChangedArgs e)
+        private static void StateObserver_StateChanged(object sender, Controller.Events.StateChangedArgs e)
         {
-            _canTakeOff = true;
-
             // state reporting interval is 5hz, so 25 should be once every 5 seconds
             if (_stateCount % 25 == 0)
             {
-                Log.WriteLine($"State: {e.State}", ConsoleColor.Green, false);
-                Log.WriteLine($"Estimated Position: { _controller.DroneState.Position}", ConsoleColor.Blue);
+                Log.WriteLine($"state: {e.State}", ConsoleColor.Yellow);
             }
             _stateCount = _stateCount < Int32.MaxValue
                 ? _stateCount + 1
                 : 0;
+
+            var group = _repository.NewEntity<ObservationGroup>(_session);
+            _repository.Insert(new StateObservation(group, e.State));
+            _repository.Insert(new AirSpeedObservation(group, e.State));
+            _repository.Insert(new AttitudeObservation(group, e.State));
+            _repository.Insert(new BatteryObservation(group, e.State));
+            _repository.Insert(new HobbsMeterObservation(group, e.State));
+            _repository.Insert(new PositionObservation(group, e.State));
+        }
+
+        private static void Controller_ExceptionThrown(object sender, Controller.Events.ExceptionThrownArgs e)
+        {
+            Log.WriteLine($"Exception {e.Exception.InnerException.GetType()} with message '{e.Exception.InnerException.Message}'", ConsoleColor.Red, false);
+            Log.WriteLine("| Stack trace", ConsoleColor.Red, false);
+            Log.WriteLine($"| {e.Exception.InnerException.StackTrace}", ConsoleColor.Red);
+        }
+
+        private static void Controller_ConnectionStateChanged(object sender, Controller.Events.ConnectionStateChangedArgs e)
+        {
+            Log.WriteLine($"Connection State: {e.Connected}");
+            if (e.Connected)
+            {
+                RunDemo();
+            }
+        }
+
+        #endregion
+
+        private static bool _canMove = false;
+        private static void RunDemo()
+        {
+            Log.WriteLine("> get battery");
+            _tello.Controller.GetBattery();
+
+            Log.WriteLine("> take off");
+            _tello.Controller.TakeOff();
+
+            var spinWait = new SpinWait();
+            while (!_canMove)
+            {
+                spinWait.SpinOnce();
+            }
+
+            Log.WriteLine("> go forward");
+            _tello.Controller.GoForward(50);
+
+            //Log.WriteLine("> turn counter clockwise");
+            //_tello.Controller.TurnCounterClockwise(90);
+
+            //Log.WriteLine("> go 50, 50, 50");
+            //_tello.Controller.Go(50, 50, -50, 50);
+
+            // sample validation exception
+            //try
+            //{
+            //    Log.WriteLine("> go forward failure example");
+            //    _tello.Controller.GoForward(10);
+            //}
+            //catch (Exception ex)
+            //{
+            //    Log.WriteLine($"GoForward failed with exception {ex.GetType()} and message '{ex.Message}'", ConsoleColor.Red);
+            //}
+
+            //Log.WriteLine("> go forward");
+            //_tello.Controller.GoForward(50);
+
+            //Log.WriteLine("> go up");
+            //_tello.Controller.GoUp(50);
+
+            //Log.WriteLine("> turn counter clockwise");
+            //_tello.Controller.TurnCounterClockwise(90);
+
+            //Log.WriteLine("> turn clockwise");
+            //_tello.Controller.TurnClockwise(45);
+
+            Log.WriteLine("> fly polygon");
+            _tello.Controller.FlyPolygon(4, 100, 50, ClockDirections.Clockwise, false);
+
+            Log.WriteLine("> land");
+            _tello.Controller.Land();
+
+            // you have to do this in real life
+            //_tello.PowerOff();
+        }
+
+        private static async void Connect()
+        {
+            // you have to do this in real life
+            //_tello.PowerOn();
+
+            Log.WriteLine("> enter sdk mode");
+            if (!await _tello.Controller.Connect())
+            {
+                Log.WriteLine("connection failed - program will be terminated");
+                Console.ReadKey(false);
+            }
+            else
+            {
+                Console.WriteLine("Remember to turn Tello off to keep it from overheating.");
+                Console.WriteLine("press any key when ready to end program...");
+                Console.ReadKey(false);
+            }
+            _repository.Dispose();
+        }
+
+        private static void Main(string[] _)
+        {
+            Console.WriteLine("Make sure Tello is powered up and you're connected to the network before continuing.");
+            Console.WriteLine("press any key when ready to continue...");
+            Console.ReadKey(false);
+
+            Connect();
         }
     }
 }
