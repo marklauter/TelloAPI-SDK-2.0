08ce37b1d22137fe05699435c24b427093e2bf95
Mark Lauter <mark.lauter@sumosoftware.com>
2019-05-18 16:34:36 -0400

    added a project to figure out how video decoding works

---
+++ src/Video.Sandbox/Video.cs
@@ -0,0 +1,14 @@
+﻿using System;
+using Windows.Media.Capture.Frames;
+
+namespace Video.Sandbox
+{
+    public class Video
+    {
+        public void f()
+        {
+            //var mfr = new MediaFrameReader();
+            
+        }
+    }
+}
